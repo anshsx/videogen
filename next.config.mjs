4ae7f37743e4de4d5478b2bbd165e0abb40b3860
anshsx <erasearch.co@gmail.com>
2024-11-10 13:44:49 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @type {import('next').NextConfig}
+import { NextConfig } from 'next';
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      fs: false, 
+      path: false,
+      crypto: false,
+    };
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
+}
 
 export default nextConfig;
